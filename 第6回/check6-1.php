5b8786018acf0b03b8332178f6f0e4c730fb1239
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2023-10-11 12:36:51 +0900

    upload task6-1files. These files can be moved each pages

---
+++ 第6回/check6-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>メイン画面</title>
+    </head>
+    <body>
+        <h1>確認画面</h1>
+        <form method="POST" action="main6-1.php">
+            <h1>投稿</h1>
+            <button type="create" class="btn btn-success" name="btn-create" value="btn-create">投稿</button>
+        </form>
+        <form method="POST" action="main6-1.php">
+            <h1>作成取消</h1>
+            <button type="create" class="btn btn-danger" name="btn-create" value="btn-create">戻る</button>
+        </form>
+    </body>
+</html>
---
+++ 第6回/create6-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>投稿記事入力画面</title>
+    </head>
+    <body>
+        <form method="POST" action="check6-1.php">
+            <h1>記事入力</h1>
+            <button type="create" class="btn btn-success" name="btn-create" value="btn-create">確認</button>
+        </form>
+        <form method="POST" action="main6-1.php">
+            <h1>作成取消</h1>
+            <button type="create" class="btn btn-danger" name="btn-create" value="btn-create">戻る</button>
+        </form>
+    </body>
+</html>
