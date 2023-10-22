fd4103d628869031c9b52fe9c945d0650e8e17ed
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2023-10-23 06:27:54 +0900

    upload task files. They are prototype code.

---
+++ 第9回/create9-1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>投稿記事入力画面</title>
+    </head>
+    <body>
+        <form method="POST" action="check9-1.php">
+            <h1>記事入力</h1>
+
+            <!-- Input field for text -->
+            <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['text'])) {
+                    $textValue = htmlspecialchars($_POST['text']);
+                    echo '<input type="text" name="text" value="' . $textValue . '">';
+                } else {
+                    echo '<input type="text" name="text" value="">';
+                }
+            ?>
+
+            <!-- Hidden fields for post array -->
+            <?php
+                if (isset($_POST['post']) && is_array($_POST['post'])) {
+                    $postJson = json_encode($_POST['post']);
+                    echo '<input type="hidden" name="postJson" value="' . htmlspecialchars($postJson) . '">';
+                }
+
+                // 配列postの中身を表示
+                echo '<pre>';
+                print_r($_POST['post']);
+                echo '</pre>';
+            ?>
+
+            <!-- <input type="hidden" name="post" value="<?= htmlspecialchars(json_encode($_POST['post'])) ?>"> -->
+            <button type="submit" class="btn btn-success" name="btn-confirm" value="btn-create">確認</button>
+        </form>
+
+        <form method="POST" action="main6-1.php">
+            <h1>作成取消</h1>
+            <button type="button" class="btn btn-danger" name="btn-cancel" value="btn-create">戻る</button>
+        </form>
+    </body>
+</html>
---
+++ 第9回/main9-1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>メイン画面</title>
+    </head>
+    <body>
+        <h1>なんちゃって掲示板</h1>
+        <form method="post" action="create9-1.php">
+            <input type="hidden" name="post" value="<?= htmlspecialchars(json_encode($_POST['post'])) ?>">
+            <button type="submit" class="btn btn-primary" name="btn-create">新規作成</button>
+        </form>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['post']) && is_array($_POST['post']))
+            {
+                echo '<h3>投稿内容</h3>';
+                foreach ($_POST['post'] as $key => $value)
+                {
+                    echo '<p>' . htmlspecialchars($value) . '</p>';
+                }
+            } else
+            {
+                echo '投稿はありません。';
+            }
+        ?>
+    </body>
+</html>
