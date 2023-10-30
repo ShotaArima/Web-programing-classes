e47da350c0ec3a6782f3768d3021f548fb3b0b78
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2023-10-30 21:00:49 +0900

    complete

--- 第11回/main11.php
+++ 第11回/main11.php
@@ -1,3 +1,45 @@
+<?php
+	$passlist = array( 'hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
+	if (!array_key_exists('user', $_POST))
+	{
+		echo_auth_page("ログイン");
+		exit;
+	}
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];
+	if ( (!array_key_exists($user, $passlist)) || $passlist[$user] != $pass)
+	{
+		echo_auth_page("パスワードが違います");
+		exit;
+	}
+	echo_hello_page($user);
+////////////////////////////////////////////////////////////////////////
+
+	function echo_auth_page($msg)
+	{
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>ページタイトル</title>
+	</head>
+	<body>
+		$msg
+		<form method="POST" action="main11.php">
+			username <input type="text" name="user" value=""><br>
+			password <input type="password" name="pass" value=""><br>
+			<button type="submit" name="login" value="login">Login</button>
+		</form>
+	</body>
+</html>
+EOT;
+	}
+////////////////////////////////////////////////////////////////////////
+
+	function echo_hello_page($who)
+	{
+echo <<<EOT
 <!DOCTYPE html>
 <html>
     <head>
@@ -64,7 +106,7 @@
                 <input type="hidden" name="transition" value="trans_new"></input>
             </form>
         </div>
-        <?php
+EOT;
 
             // SQLの名称定義
             $hostname='127.0.0.1';
@@ -118,6 +160,9 @@
 
             // データベースを切断
             mysqli_close($link);
-        ?>
+echo <<<EOT
     </body>
 </html>
+EOT;
+}
+?>
